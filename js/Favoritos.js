6eefa270c5d8fde7da8ef725b869e3b785f7a475
Nico <1109187838@u.icesi.edu.co>
2025-06-11 17:05:18 -0500

    Ajustes A Iconos en catalogo y favs

--- js/Favoritos.js
+++ js/Favoritos.js
@@ -27,6 +27,40 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+  // Creación de filas de tarjetas y su contenido __________________________________________________________________________________
+  for (let i = 0; i < lista.length; i += 3) {
+    const fila = document.createElement("div");
+    fila.className = "container-1"; // Clase para cada fila de 3 elementos
+
+    for (let j = i; j < i + 3 && j < lista.length; j++) {
+      const p = lista[j];
+
+      const tarjeta = document.createElement("div");
+      tarjeta.className = "element"; // Clase para cada tarjeta
+
+      // Verificar si el personaje está en favoritos
+      const estaEnFavoritos = favoritos.includes(p.id);
+
+      // Cambiar la imagen de la estrella
+      const estrellaImagen = estaEnFavoritos ? "../Img/estrella-fav.png" : "../Img/estrella.png";
+
+      tarjeta.innerHTML = `
+        <div><img src="${p.image}" alt="${p.name}" width="340px" height="450px"></div>
+        <div id="texto"><h2 id="h2b">${p.name.toUpperCase()}</h2></div>
+        <div id="boto-estrella">
+          <div><button id="Boton" onclick="verDetalle(${p.id})">MÁS DETALLES</button></div>
+          <div id="estrella">
+            <img src="${estrellaImagen}" alt="favorito" width="55px" onclick="agregarOEliminarDeFavoritos(${p.id}, '${p.name}', '${p.image}', this)">
+          </div>
+        </div>
+      `;
+
+      fila.appendChild(tarjeta);
+    }
+
+    contenedor.appendChild(fila);
+  }
+
 
 function verDetalle(id, nombre) {
   window.location.href = `Element.html?id=${id}&nombre=${encodeURIComponent(nombre)}`;
